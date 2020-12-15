2d4549d397b16357956942920d940ef705008c9a
owlhoo <anes1996_h@hotmail.com>
2020-12-16 00:35:00 +0100

    add encrypting and creating encrypted file

--- app/src/main/java/com/example/cryptguard/ui/encrypter/Encrypter.kt
+++ app/src/main/java/com/example/cryptguard/ui/encrypter/Encrypter.kt
@@ -10,15 +10,17 @@ import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.PBEKeySpec
 
 object Encrypter {
+    // Encrypt the data from stream, encode it to base64 and append salt and iv to it (base64 too)
     @RequiresApi(Build.VERSION_CODES.O)
     fun encryptAndGetBase64(input: InputStream, password: String): String {
+        println(password)
         // create the salt
         val sr = SecureRandom.getInstanceStrong()
-        val salt = ByteArray(16)
-        sr.nextBytes(salt)
+        val passwordSalt = ByteArray(16)
+        sr.nextBytes(passwordSalt)
 
         // create the key for aes
-        val keySpec = PBEKeySpec(password.toCharArray(), salt, 1000, 32 * 8)
+        val keySpec = PBEKeySpec(password.toCharArray(), passwordSalt, 1000, 32 * 8)
         val key = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512").generateSecret(keySpec)
 
         // create the cipher and initialise it with key
@@ -32,8 +34,24 @@ object Encrypter {
             result = cipher.doFinal(bytes)
         }
 
-        // return the data in base64
-        return Base64.getEncoder().encodeToString(result)
+        // return the encrypted data in base64
+        return appendCipherDataToBase64(result, passwordSalt, cipher.iv)
+    }
+
+    private fun ByteArray.toBase64(): String {
+        return android.util.Base64.encodeToString(this, android.util.Base64.DEFAULT).toString()
+    }
+
+    private fun ByteArray.fromBase64(): ByteArray? {
+        return android.util.Base64.decode(this, android.util.Base64.DEFAULT)
+    }
+
+    private fun appendCipherDataToBase64( cipherText: ByteArray, passwordSalt: ByteArray, initVector: ByteArray): String {
+        val cipherBase64 = cipherText.toBase64()
+        val passwordSaltBase64 = passwordSalt.toBase64()
+        val initVectorBase64 = initVector.toBase64()
+
+        return cipherBase64.plus('\n' + passwordSaltBase64 + '\n' + initVectorBase64)
     }
 
     fun decrypt(input: InputStream, password: String) {
--- app/src/main/java/com/example/cryptguard/ui/encrypter/EncrypterFragment.kt
+++ app/src/main/java/com/example/cryptguard/ui/encrypter/EncrypterFragment.kt
@@ -3,16 +3,21 @@ package com.example.cryptguard.ui.encrypter
 import android.app.Activity.RESULT_OK
 import android.content.ContentValues
 import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.provider.OpenableColumns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.cryptguard.R
+import kotlinx.android.synthetic.main.fragment_encrypter.*
 import kotlinx.android.synthetic.main.fragment_encrypter.view.*
 
 
@@ -21,15 +26,18 @@ class EncrypterFragment : Fragment() {
     private lateinit var encrypterViewModel: EncrypterViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         encrypterViewModel =
                 ViewModelProvider(this).get(EncrypterViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_encrypter, container, false)
 
         root.encrypt_file_button.setOnClickListener {
+            if (!passwordEditTextValidate())
+                return@setOnClickListener
+
             val intent = Intent()
                     .setType("*/*")
                     .setAction(Intent.ACTION_GET_CONTENT)
@@ -38,6 +46,9 @@ class EncrypterFragment : Fragment() {
         }
 
         root.decrypt_file_button.setOnClickListener {
+            if (!passwordEditTextValidate())
+                return@setOnClickListener
+
             val intent = Intent()
                 .setType("*/*")
                 .setAction(Intent.ACTION_OPEN_DOCUMENT_TREE)
@@ -55,19 +66,30 @@ class EncrypterFragment : Fragment() {
         // TODO: implement encrypting/decrypting and creating file
         if (requestCode == 1 && resultCode == RESULT_OK) {
             data?.data?.let {
-                // reads a file
-                val line = context?.contentResolver?.openInputStream(it)?.bufferedReader().use { it!!.readText() }
-
-                // creates a file
-                val resolver = context?.contentResolver
-                val contentValues = ContentValues().apply {
-                    put(MediaStore.MediaColumns.DISPLAY_NAME, "test.txt")
+                var encryptedBase64: String = ""
+                val line = context?.contentResolver?.openInputStream(it)?.buffered().use { fileStream ->
+                    if (fileStream != null) {
+                        encryptedBase64 = Encrypter.encryptAndGetBase64(fileStream, this.password_encrypter_edit_text.text.toString())
+                    }
                 }
 
-                val uri = resolver?.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, contentValues)
+                if (encryptedBase64 == "") {
+                    Toast.makeText(context, "Failed encrypting a selected file.", Toast.LENGTH_LONG).show()
+                    return
+                }
 
-                resolver!!.openOutputStream(uri!!).use {
-                    it?.write("hello".toByteArray())
+                // creates values of a to be file
+                val resolver = context?.contentResolver
+                val contentValues = ContentValues().apply {
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, getFileName(it) + ".cgfe")
+                }
+
+                // creates the file and writes the data into it
+                val uri = resolver?.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, contentValues)
+                if (uri != null) {
+                    resolver.openOutputStream(uri)?.writer().use {
+                        it?.write(encryptedBase64)
+                    }
                 }
             }
         } else if (requestCode == 2 && resultCode == RESULT_OK) {
@@ -88,5 +110,40 @@ class EncrypterFragment : Fragment() {
                 }
             }
         }
+    }
+
+    private fun passwordEditTextValidate(): Boolean {
+        val passwordText = this.password_encrypter_edit_text.text
+        if (passwordText.trim().isEmpty() || passwordText.length < 8) {
+            this.password_encrypter_edit_text.setError("Password length has to be more than 8 characters.")
+            return false
+        }
+        return true
+    }
+
+    private fun getFileName(uri: Uri): String? {
+        var result: String? = null
+        if (uri.scheme.equals("content")) {
+            val cursor: Cursor? = context?.contentResolver?.query(uri, null, null, null, null)
+
+            cursor.use {
+                if (it != null && it.moveToFirst()) {
+                    if (cursor != null) {
+                        result = it.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                    }
+                }
+            }
+        }
+
+        if (result == null) {
+            result = uri.path
+            val cut = result?.lastIndexOf('/')
+            if (cut != -1) {
+                if (result != null) {
+                    result = cut?.plus(1)?.let { result!!.substring(it) }
+                }
+            }
+        }
+        return result
     }
 }
